ac6cec34437d7135a0815e5936cc2e4a61bd0195
Joemillard <joeymillard@hotmail.co.uk>
2021-10-26 16:20:46 +0100

    correcting column order

--- R/04_derive_random_raw_trends.R
+++ R/04_derive_random_raw_trends.R
@@ -56,19 +56,25 @@ clusterEvalQ(cl, {
 # set up the function for calculating trends
 run_SAI_change <- function(views){
   
+  # arrange views by date
+  views <- views %>%
+    group_by(q_wikidata) %>%
+    arrange(date) %>%
+    ungroup()
+  
   # convert to wide format
   views_wide <- tidyr::pivot_wider(views, 
                                    names_from = c(year, month), 
                                    values_from = av_views, 
                                    id_cols=c(year, month, av_views, q_wikidata))
   
-  # save names of columns containing av_views values in a variable
-  colnames_var <- colnames(views_wide[,2:length(views_wide)])
-  
   # move the q_wikidata column to the end
   views_wide <- relocate(views_wide, 
                          q_wikidata, 
                          .after = last_col())
+  
+  # save names of columns containing av_views values in a variable
+  colnames_var <- colnames(views_wide[,2:length(views_wide)])
   
   # create a variable with temporary numeric column names
   m_colnames <- 1:(length(views_wide)-1)
@@ -86,17 +92,15 @@ run_SAI_change <- function(views){
   # add a unique species id
   views_wide$SpecID <- seq_along(views_wide$q_wikidata)
   
-  # remove all time series that do not meet minimum length and observation # thresholds
-  views_wide_culled <- cull_fn(views_wide, 
-                               count_thres=2, 
-                               min_ts_length=57, columns) %>%
-    mutate(copied = 1)
+  # remove any rows with NA and add 1 for following function
+  views_wide <- views_wide[complete.cases(views_wide), ]
+  views_wide$copied <- 1
   
   # GAM the population indices, checking the fit and marking those that dont fit well
-  views_gammed <- gam_fn(views_wide_culled, 
+  views_gammed <- gam_fn(views_wide, 
                          columns, 
                          m_colnames)
-  
+
   # log-linear interpolate populations that had poorly fitting GAMs
   views_complete <- chain_fn(views_gammed, 
                              columns, 
