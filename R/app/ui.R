71f59a17903f47516f8df6aa0313eb2ac12480e8
Joemillard <joeymillard@hotmail.co.uk>
2021-12-01 14:44:50 +0000

    update app to server and ui

--- R/app/ui.R
+++ R/app/ui.R
@@ -6,7 +6,7 @@ panel_div <- function(class_type, content) {
   )
 }
 
-ui <- shinyUI(navbarPage(title=div(tags$a(href="",img(src="zsl_logo.png"), "")), id = "navBar",
+shinyUI(navbarPage(title=div(tags$a(href="",img(src="zsl_logo.png"), "")), id = "navBar",
                          theme = "paper.css",
                          collapsible = TRUE,
                          inverse = TRUE,
@@ -123,15 +123,14 @@ ui <- shinyUI(navbarPage(title=div(tags$a(href="",img(src="zsl_logo.png"), "")),
                                            div(class="panel panel-default",
                                                div(class="panel-body",  
                                                    tags$div(align = "center",
-                                                             div(align = "center", 
-                                                                  h5("About the team")
-                                                             )
+                                                            div(align = "center", 
+                                                                h5("About the team")
+                                                            )
                                                    ),
                                                    tags$p(h6(style="text-align: justify;", "The Species Awareness Index was originally conceived and created by Joe Millard, Robin Freeman, Richard Gregory, and Kate Jones (see the original publication here). 
                                                              Since its initial publication, the underlying code has been revised further by Joe Millard and Shawn Dove. The near real-time version hosted on this site was then built in collaboration with Robin Freeman, Thomas Frederick Johnson, and Richard Cornford.")),
                                                    
                                                    fluidRow(
-                                                     
                                                      # Rob
                                                      column(2,
                                                             div(class="panel panel-default", 
@@ -148,7 +147,6 @@ ui <- shinyUI(navbarPage(title=div(tags$a(href="",img(src="zsl_logo.png"), "")),
                                                                 )
                                                             )
                                                      ),
-                                                     
                                                      # Joe
                                                      column(2,
                                                             div(class="panel panel-default",
@@ -160,18 +158,55 @@ ui <- shinyUI(navbarPage(title=div(tags$a(href="",img(src="zsl_logo.png"), "")),
                                                                     ),
                                                                     div(
                                                                       tags$h5("Joe Millard"),
-                                                                      tags$h6(tags$i("PhD Research Student"))
+                                                                      tags$h6(tags$i("Postdoctoral researcher, Leverhulme Centre for Demographic Science"))
                                                                     )
                                                                 )
                                                             )
                                                      ),
-                                                     column(3)
-                                                   )
-                                               )
-                                           ) # Closes div panel
-                                    ), # Closes column
-                                    column(2)
-                                  ),
+                                                     # Shawn
+                                                     column(2,
+                                                            div(class="panel panel-default",
+                                                                div(class="panel-body",    style = "height:300px", 
+                                                                    align = "center",
+                                                                    div(
+                                                                      tags$img(src = "shawn_dove.jfif", align = "center",
+                                                                               width = "90px", height = "90px")
+                                                                    ),
+                                                                    div(
+                                                                      tags$h5("Shawn Dove"),
+                                                                      tags$h6(tags$i("PhD student, IoZ & UCL"))
+                                                                    )
+                                                                )
+                                                            )),
+                                                     column(2,
+                                                            div(class="panel panel-default",
+                                                                div(class="panel-body",    style = "height:300px", 
+                                                                    align = "center",
+                                                                    div(
+                                                                      tags$img(src = "tom_johnson.jpg", align = "center",
+                                                                               width = "90px", height = "90px")
+                                                                    ),
+                                                                    div(
+                                                                      tags$h5("Thomas Frederick-Johnson"),
+                                                                      tags$h6(tags$i("Postdoctoral researcher, Sheffield University"))
+                                                                    )
+                                                                )
+                                                            )),
+                                                     column(2,
+                                                            div(class="panel panel-default",
+                                                                div(class="panel-body",    style = "height:300px", 
+                                                                    align = "center",
+                                                                    div(
+                                                                      tags$img(src = "rich_placeholder.jpg", align = "center",
+                                                                               width = "90px", height = "90px")
+                                                                    ),
+                                                                    div(
+                                                                      tags$h5("Richard Cornford"),
+                                                                      tags$h6(tags$i("PhD student, IoZ & UCL"))
+                                                                    )
+                                                                )
+                                                            ))
+                                                   ))))),
                                   fluidRow(
                                     column(2),
                                     column(8,
@@ -255,113 +290,3 @@ ui <- shinyUI(navbarPage(title=div(tags$a(href="",img(src="zsl_logo.png"), "")),
                          )  # Closes the second tabPanel called "Download"
 )
 )
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-  
-  library(ggplot2)
-  library(dplyr)
-  
-  class_language <- readRDS("class_language.rds")
-  class_language_change <- readRDS("class_language_change.rds")
-  class_SAI <- readRDS("class_trend.rds")
-  overall_SAI <- readRDS("overall.rds")
-  
-  # need to make requests of our SAI API here, which pulls in a set of dataframes:
-  # overall_SAI, class_SAI, class_language_SAI, class_language_change
-  
-  # plot of total production vulnerability for each country
-  output$overall_SAI <-  renderPlot({
-    
-    # main text plot -- collapse together the average lambda at each start point for each class, add last row for value 57, and then stick LPI values back on
-    overall_SAI %>%
-      ggplot() +
-      geom_ribbon(aes(x = Year, ymin = LPI_lwr, ymax = LPI_upr), alpha = 0.3) +
-      geom_line(aes(x = Year, y = LPI)) +
-      geom_hline(yintercept = 1, linetype = "dashed", size = 1) +
-      scale_y_continuous(breaks = c(1.05, 1, 0.95, 0.9, 0.85, 0.8), labels = c("1.05","1", "0.95", "0.9", "0.85", "0.8")) +
-      ylab("") +
-      xlab(NULL) +
-      theme_bw() +
-      theme(panel.grid = element_blank(), legend.position = "none",
-            text = element_text(size = 16))
-    
-  })
-  
-  # plot of total production vulnerability for each country
-  output$class_SAI <-  renderPlot({
-    
-    # plot all the class level trends with point for whether increasing or decreasing
-    class_SAI %>%
-      ggplot() +
-      geom_ribbon(aes(x = Year, ymin = LPI_lwr, ymax = LPI_upr, fill = taxa), alpha = 0.3) +
-      geom_line(aes(x = Year, y = LPI, colour = taxa), size = 1) +
-      geom_hline(yintercept = 1, linetype = "dashed", size = 1) +
-      scale_colour_manual("Taxonomic class", na.translate = F, values = c("black", "#FF7F00", "#377EB8", "#4DAF4A", "#F781BF", "#A65628")) +
-      scale_fill_manual("Taxonomic class", na.translate = F, values = c("black", "#FF7F00", "#377EB8", "#4DAF4A", "#F781BF", "#A65628")) +
-      scale_y_continuous(breaks = c(0.8, 1, 1.2), labels = c("0.8", "1", "1.2")) +
-      facet_wrap(~taxa) +
-      ylab("") +
-      xlab(NULL) +
-      theme_bw() +
-      theme(panel.grid = element_blank(), legend.position = "none",
-            text = element_text(size = 16))
-    
-  })
-  
-  # plot of total production vulnerability for each country
-  output$class_language_SAI <-  renderPlot({
-    
-    # plot all the class level trends
-    class_language %>%
-      mutate(language = factor(language, levels = c("Arabic", "Chinese", "English", "French", "German", "Italian", "Japanese", "Portuguese", "Russian", "Spanish"),
-                               labels = c("Ar", "Ch", "En", "Fr", "Ge", "It", "Ja", "Po", "Ru", "Sp"))) %>%
-      ggplot() +
-      geom_ribbon(aes(x = Year, ymin = LPI_lwr, ymax = LPI_upr, fill = taxa), alpha = 0.4) +
-      geom_line(aes(x = Year, y = LPI, colour = taxa)) +
-      geom_hline(yintercept = 1, linetype = "88", size = 0.3, alpha = 0.3) +
-      scale_fill_manual("Taxonomic class", values = c("black", "#FF7F00", "#377EB8", "#4DAF4A", "#F781BF", "#A65628")) +
-      scale_colour_manual("Taxonomic class", values = c("black", "#FF7F00", "#377EB8", "#4DAF4A", "#F781BF", "#A65628")) +
-      scale_y_continuous(breaks = c(0, 0.5, 1, 1.5), labels = function(x) ifelse(x == 0, "0", x)) +
-      facet_grid(language~taxa) +
-      ylab("") +
-      xlab(NULL) +
-      theme_bw() +
-      theme(panel.grid = element_blank(),
-            axis.text.y = element_blank(),
-            axis.text.x = element_blank(), 
-            axis.title.x = element_blank(),
-            axis.ticks.x = element_blank(),
-            axis.ticks.y = element_blank(), 
-            legend.position = "none",
-            text = element_text(size = 16),
-            strip.text.y = element_text(angle = 45))
-    
-  })
-  
-  # plot of total production vulnerability for each country
-  output$class_language_change <-  renderPlot({
-    
-    # add labels for factors, sort by predicted value for language and class, and then plot
-    class_language_change %>%
-      ggplot() + 
-      geom_hline(yintercept = 0, linetype = "dashed", size = 1, colour = "grey") +
-      geom_errorbar(aes(x = 1, colour = taxa, y = predicted_values, ymin = (predicted_values - (1.96 * predicted_values_se)), ymax = (predicted_values + (1.96 * predicted_values_se))), position=position_dodge(width = 0.5), width = 0.2) +
-      geom_point(aes(x = 1, colour = taxa, y = predicted_values), position=position_dodge(width=0.5)) +
-      ylab("Monthly change in SAI") +
-      facet_wrap(~language) +
-      scale_y_continuous(breaks = c(-0.015, -0.01, -0.005, 0, 0.005), labels = c("-0.015", "-0.010", "-0.005", "0", "0.005")) +
-      scale_colour_manual("Taxonomic class", values = c("black", "#FF7F00", "#377EB8", "#4DAF4A", "#F781BF", "#A65628")) +
-      theme_bw() +
-      theme(panel.grid = element_blank(),
-            axis.text.x = element_blank(), 
-            axis.title.x = element_blank(),
-            axis.ticks.x = element_blank(), legend.position = "none",
-            text = element_text(size = 16))
-    
-  })
-  
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
